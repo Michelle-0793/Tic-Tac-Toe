f5ebb430e7cfb88eed68945ebc676d2d7325802e
Michelle-0793 <mlopez@fwdcostarica.com>
2024-08-06 15:42:51 -0600

    avance de etapa 2

--- src/TicTacToe.js
+++ src/TicTacToe.js
@@ -1,6 +1,52 @@
 const celdas=document.getElementsByClassName("celda");
 
 
+//JUEGA LA PERSONA
+//Recorro todas las celdas
+for (let index = 0; index < celdas.length; index++) {
+    //agrego el evento
+    celdas[index].addEventListener("click", function () {
+        //verifico si está vacía la celda
+        if (celdas[index].textContent === "") {
+            celdas[index].textContent = "X";  // Coloca X en la celda seleccionada
+        }   
+
+//JUEGA LA COMPU
+        // Escoger una celda vacía para la computadora
+        let celdasVacias = []; //Guardo todas las celdas que quedaron vacías después de que la persona jugó
+
+//Se vuelve a recorrer para ahora guardar las celdas vacías
+        for (let index = 0; index < celdas.length; index++) {
+            if (celdas[index].textContent === "") {
+                celdasVacias.push(celdas[index]);
+            }
+        }
+
+//La compu elija al azar
+//Si hay vacías, genera aleatorio entre 0 y el # de celdas que están vacías
+        if (celdasVacias.length > 0) { //Comparo la cantidad de celdas vacías es mayor ">" que 0
+            //si esto no es cierto no se ejecuta, la compu no juega
+            let numeroAleatorio = Math.floor(Math.random() * celdasVacias.length); 
+            celdasVacias[numeroAleatorio].textContent = "O"; //la que está vacía se llena con una "O"
+        }
+    });
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 for (let index = 0; index < celdas.length; index++) {
 
     celdas[index].addEventListener("click", function () {
@@ -14,20 +60,14 @@ let celdasVacias = []
 
 for (let index = 0; index < celdas.length; index++) {
     if (celdas[index].textContent==="") {
-        celdasVacias.push(index);
+        celdasVacias.push(celdas[index]);
     }
 }
 
 if (celdasVacias.length > 0) {
-    let numeroAleatorio = Math.floor(Math.random() * celdasVacias.length); 
+    let numeroAleatorio = Math.floor(Math.random() * celdas.length); 
     celdas[numeroAleatorio].textContent = "O"; 
 }
-
-
-
-
-
-
 
     if (celdas[numeroAleatorio].textContent === "") {
         celdas[numeroAleatorio].textContent = "O"; //sino colocar O
@@ -46,4 +86,4 @@ if (celdasVacias.length > 0) {
     }       
     })
 }
-
+*/
