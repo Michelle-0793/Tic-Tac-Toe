eab63d2e5aaf46709ad62808afe926c98a75b0f0
Michelle-0793 <mlopez@fwdcostarica.com>
2024-08-07 11:56:23 -0600

    estilos

--- src/TicTacToe.js
+++ src/TicTacToe.js
@@ -7,21 +7,26 @@ const celdas=document.getElementsByClassName("celda");
 for (let index = 0; index < celdas.length; index++) {
     //agrego el evento
     celdas[index].addEventListener("click", function () {
+        console.log(celdas[index])
         //verifico si está vacía la celda
         if (celdas[index].textContent === "") {
             celdas[index].textContent = "X";  // Coloca X en la celda seleccionada
-
+            celdas[index].style.backgroundColor= "#FFE3FD"
+    
+console.log(celdas[index]);
 
 //JUEGA LA COMPU
 // Escoger una celda vacía para la computadora
     let celdasVacias = []; //Guardo todas las celdas que quedaron vacías después de que la persona jugó
-    
+
     //Se vuelve a recorrer para ahora guardar las celdas vacías
     for (let index = 0; index < celdas.length; index++) {
         if (celdas[index].textContent === "") {
-                        celdasVacias.push(celdas[index]);
+        celdasVacias.push(celdas[index]);
         }
-        }
+    }
+
+console.log(celdasVacias);
         
 //La compu elija al azar
 //Si hay vacías, genera aleatorio entre 0 y el # de celdas que están vacías
@@ -29,12 +34,13 @@ for (let index = 0; index < celdas.length; index++) {
         //si esto no es cierto no se ejecuta, la compu no juega
          let numeroAleatorio = Math.floor(Math.random() * celdasVacias.length); 
         celdasVacias[numeroAleatorio].textContent = "O"; //la que está vacía se llena con una "O"
-        celdasVacias[numeroAleatorio].onclick = null
+
+console.log(numeroAleatorio);
+console.log(celdas);
+
         }
         }   
-
-
-    });
+    })
 }
 
 
