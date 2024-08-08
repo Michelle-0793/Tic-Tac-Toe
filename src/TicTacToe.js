c6d541a414e7c2d197f3bfef8fcb43b5dff5eca5
Michelle-0793 <mlopez@fwdcostarica.com>
2024-08-08 09:54:30 -0600

    implementación de pointerEvents, para que cuando alguien gane ya no se pueda hacer click en otra celda

--- src/TicTacToe.js
+++ src/TicTacToe.js
@@ -11,35 +11,38 @@ for (let index = 0; index < celdas.length; index++) {
         console.log(celdas[index])
         //verifico si está vacía la celda
         if (celdas[index].textContent === "") {
-            celdas[index].textContent = "X";  // Coloca X en la celda seleccionada
+            celdas[index].textContent = "🤍";  // Coloca X en la celda seleccionada
             celdas[index].style.backgroundColor= "#FFE3FD"
     
 console.log(celdas[index]);
 
 //Verifico si la persona gana
 const combinacionesGanadoras= [
-    [0,1,2], //filas
+    [0,1,2],
     [3,4,5],
     [6,7,8],
-    [0,3,6], //columnas
+    [0,3,6], 
     [1,4,7],
     [2,5,8],
-    [0,4,8], //Diagonales
+    [0,4,8], 
     [2,4,6],
 ]
 //un for para recorrer todas esa combinaciones
 for (let index = 0; index < combinacionesGanadoras.length; index++) { 
     const combinación = combinacionesGanadoras[index] //cada que itera esa varieble obtiene una combinación
 // agrego cada elemento a una varieble, separo
-    const a = combinación [0];
-    const b = combinación [1];
-    const c = combinación [2];
+    const indiceA = combinación [0];
+    const indiceB = combinación [1];
+    const indiceC = combinación [2];
 
 //valido, para ver si las celdas correspondientes a los índices a, b y c tienen una "X"
-    if (celdas[a].textContent === "X" && celdas[b].textContent === "X" && celdas[c].textContent === "X") {
-        aviso.textContent = "¡Ganaste!"
+    if (celdas[indiceA].textContent === "🤍" && celdas[indiceB].textContent === "🤍" && celdas[indiceC].textContent === "🤍") {
+        aviso.textContent = "¡Ganaste!";
+        terminarJuego()
         break; 
+        
     }
+
 }
 
 //JUEGA LA COMPU
@@ -53,30 +56,31 @@ for (let index = 0; index < combinacionesGanadoras.length; index++) {
         }
     }
 
-console.log(celdasVacias);
         
 //La compu elija al azar
 //Si hay vacías, genera aleatorio entre 0 y el # de celdas que están vacías
         if (celdasVacias.length > 0) { //Comparo la cantidad de celdas vacías es mayor ">" que 0
         //si esto no es cierto no se ejecuta, la compu no juega
          let numeroAleatorio = Math.floor(Math.random() * celdasVacias.length); 
-        celdasVacias[numeroAleatorio].textContent = "O"; //la que está vacía se llena con una "O"
+         celdasVacias[numeroAleatorio].textContent = "◯"; //la que está vacía se llena con una "O"
 
         for (let index = 0; index < combinacionesGanadoras.length; index++) {
             const combinación = combinacionesGanadoras[index]
-            const a = combinación [0];
-            const b = combinación [1];
-            const c = combinación [2];
+            const indiceA = combinación [0];
+            const indiceB = combinación [1];
+            const indiceC = combinación [2];
         
         
-            if (celdas[a].textContent === "O" && celdas[b].textContent === "O" && celdas[c].textContent === "O") {
-                aviso.textContent = "¡Sigue intentándolo!"
+            if (celdas[indiceA].textContent === "◯" && celdas[indiceB].textContent === "◯" && celdas[indiceC].textContent === "◯") {
+                aviso.textContent = "¡Sigue intentándolo!";
+                terminarJuego()
                 break; 
             }
         }
        
         }
         empate(); //llamo a la función empate
+
         }   
     })
 }
@@ -91,7 +95,7 @@ function empate() { // recorre todas las celdas para ver si alguna está vacía
          celdasLlenas = false; //si encontró celda vacía, ahora es falso
          break; 
      }
-     console.log(empate);
+
      
     }
  //EMPATE
@@ -100,18 +104,28 @@ function empate() { // recorre todas las celdas para ver si alguna está vacía
     if (celdasLlenas && aviso.textContent === "") { 
      aviso.textContent = "¡Empate!"
     }
+
+
  }
 
  //REINICIAR
 iconoReiniciar.addEventListener("click", function () {
     for (let index = 0; index < celdas.length; index++) {
         celdas[index].textContent = ""; //limpia el contenido de la celda
-        celdas[index].style.backgroundColor= "" //restablece el color
+        celdas[index].style.backgroundColor = "" //restablece el color
     }
     aviso.textContent = "" //limpia el aviso
 })
 
 
+//TERMINAR JUEGO
+function terminarJuego() {
+    for (let index = 0; index < celdas.length; index++) {
+        if (aviso.textContent === "¡Ganaste!" || aviso.textContent === "¡Sigue intentándolo!") {
+            celdas[index].style.pointerEvents = "none";
+        }   
+    }
+}
 
 
 
